d4ecf423f854f53ef2c75fedd7c3017e7cda0bdf
sixer01 <spike723224@gmail.com>
2022-04-17 02:38:02 +0300

    tests(checkbox, questions) & login verst

---
+++ Technical task/wwwroot/js/testb1.js
@@ -0,0 +1,24 @@
+﻿let questions = [
+    {
+        numb: 1,
+        question: "__________ contains information about the assembly and the resources that it depends on",
+        answer: "Assembly manifest",
+        options: [
+            "MSIL",
+            "Assembly manifest",
+            "GAC",
+            "Type metadata"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What are the configuration files used by the .Net Framework?",
+        answer: "All of the above",
+        options: [
+            "Machine configuration, Machine.config file",
+            "Application configuration file",
+            "Security configuration file",
+            "All of the above"
+        ]
+    },
+];
